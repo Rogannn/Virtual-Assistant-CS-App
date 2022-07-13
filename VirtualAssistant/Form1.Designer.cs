346887800b582f53b60f4918ee8662d2446ebcd9
Rogelio L. Haban Jr <93583860+Rogannn@users.noreply.github.com>
2022-07-13 19:15:10 +0800

    initial commit

---
+++ VirtualAssistant/Form1.Designer.cs
@@ -0,0 +1,94 @@
+﻿
+namespace VirtualAssistant
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.ListCommands = new System.Windows.Forms.ListBox();
+            this.SpeakTimer = new System.Windows.Forms.Timer(this.components);
+            this.SpeakBar = new System.Windows.Forms.ProgressBar();
+            this.flowLayoutPanel1 = new System.Windows.Forms.FlowLayoutPanel();
+            this.SuspendLayout();
+            // 
+            // ListCommands
+            // 
+            this.ListCommands.FormattingEnabled = true;
+            this.ListCommands.Location = new System.Drawing.Point(3, 42);
+            this.ListCommands.Name = "ListCommands";
+            this.ListCommands.Size = new System.Drawing.Size(96, 342);
+            this.ListCommands.TabIndex = 0;
+            this.ListCommands.Visible = false;
+            // 
+            // SpeakTimer
+            // 
+            this.SpeakTimer.Enabled = true;
+            this.SpeakTimer.Interval = 1000;
+            this.SpeakTimer.Tick += new System.EventHandler(this.SpeakTimer_Tick);
+            // 
+            // SpeakBar
+            // 
+            this.SpeakBar.Location = new System.Drawing.Point(156, 367);
+            this.SpeakBar.Name = "SpeakBar";
+            this.SpeakBar.Size = new System.Drawing.Size(368, 15);
+            this.SpeakBar.TabIndex = 1;
+            // 
+            // flowLayoutPanel1
+            // 
+            this.flowLayoutPanel1.AutoScroll = true;
+            this.flowLayoutPanel1.FlowDirection = System.Windows.Forms.FlowDirection.BottomUp;
+            this.flowLayoutPanel1.Location = new System.Drawing.Point(156, 42);
+            this.flowLayoutPanel1.Name = "flowLayoutPanel1";
+            this.flowLayoutPanel1.Size = new System.Drawing.Size(368, 319);
+            this.flowLayoutPanel1.TabIndex = 4;
+            this.flowLayoutPanel1.WrapContents = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(658, 419);
+            this.Controls.Add(this.flowLayoutPanel1);
+            this.Controls.Add(this.SpeakBar);
+            this.Controls.Add(this.ListCommands);
+            this.Name = "Form1";
+            this.Text = "Virtual Assistant";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox ListCommands;
+        private System.Windows.Forms.Timer SpeakTimer;
+        private System.Windows.Forms.ProgressBar SpeakBar;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel1;
+    }
+}
+
---
+++ VirtualAssistant/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Speech.Recognition;
+using System.Speech.Synthesis;
+using System.IO;
+
+namespace VirtualAssistant
+{
+    public partial class Form1 : Form
+    {
+        // underscore means its global
+        SpeechRecognitionEngine _recognizer = new SpeechRecognitionEngine();
+        SpeechSynthesizer VoiceAssistant = new SpeechSynthesizer();
+        // this is optional, only to be able to start and stop the speech recognition
+        SpeechRecognitionEngine startListening = new SpeechRecognitionEngine();
+
+        Random rand = new Random();
+        // so the assistant will be able to respond to a new speech after a period of time
+        int RecTimeOut = 0;
+
+        DateTime CurrentTime = DateTime.Now;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            _recognizer.SetInputToDefaultAudioDevice();
+            _recognizer.LoadGrammarAsync(
+                new Grammar(
+                    new GrammarBuilder(
+                        new Choices(File.ReadAllLines(@"Speeches.txt")))));
+            _recognizer.SpeechRecognized += new EventHandler
+                <SpeechRecognizedEventArgs>(Default_SpeechRecognized);
+            _recognizer.SpeechDetected += new EventHandler
+                <SpeechDetectedEventArgs>(_recognizer_SpeechRecognized);
+            _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            _recognizer.AudioLevelUpdated += new EventHandler
+                <AudioLevelUpdatedEventArgs>(_recognizer_AudioLevelUpdated);
+
+            startListening.SetInputToDefaultAudioDevice();
+            startListening.LoadGrammarAsync(
+                new Grammar(
+                    new GrammarBuilder(
+                        new Choices(File.ReadAllLines(@"Speeches.txt")))));
+            startListening.SpeechRecognized += new EventHandler
+                <SpeechRecognizedEventArgs>(startListening_SpeechRecognized);
+        }
+
+        private void Default_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            int randNum;
+            string speech = e.Result.Text;
+            UserSpeechBubble(speech);
+
+            if (speech == "hello" || speech == "hi" || speech == "greetings")
+            {
+                string voice = "Hello, I am your Virtual Assistant. \nHow can I help you?";
+                AssistantSpeechBubble(voice, true);
+            }
+            if (speech == "how are you")
+            {
+                string voice = "I am working fine, thank you for asking.";
+                AssistantSpeechBubble(voice, true);
+            }
+            if (speech == "what time is it" || speech == "what is the current time")
+            {
+                // string getTime = DateTime.Now.ToString("h: mm tt");
+                string getTime = DateTime.Now.ToString("h: mm tt");
+                string est = "";
+                if (getTime.Contains("pm"))
+                {
+                    est = " in the evening.";
+                }
+                if (getTime.Contains("am"))
+                {
+                    est = " in the morning.";
+                }
+                string voice = "It is currently " + getTime + est;
+                AssistantSpeechBubble(voice, true);
+            }
+            // to abruptly stop the voice from speaking
+            if (speech == "stop talking")
+            {
+                VoiceAssistant.SpeakAsyncCancelAll();
+                // randomly choose which voice to respond
+                randNum = rand.Next(1, 2);
+                if (randNum == 1)
+                {
+                    string voice = "Ok.";
+                    AssistantSpeechBubble(voice, true);
+                }
+                if (randNum == 2)
+                {
+                    string voice = "Yes?";
+                    AssistantSpeechBubble(voice, true);
+                }
+            }
+            if (speech == "stop listening")
+            {
+                string voice = "I will shut my ears now. Just say \"Start\" and I will hear you again.";
+                AssistantSpeechBubble(voice, true);
+                _recognizer.RecognizeAsyncCancel();
+                startListening.RecognizeAsync(RecognizeMode.Multiple);
+            }
+            if (speech == "show speeches" || speech == "show available speeches" || speech == "what are the available speeches")
+            {
+                string voice = "Showing all the available speeches..";
+                AssistantSpeechBubble(voice, true);
+                string[] speeches = (File.ReadAllLines(@"Speeches.txt"));
+                ListCommands.Items.Clear();
+                ListCommands.SelectionMode = SelectionMode.None;
+                ListCommands.Visible = true;
+                foreach (string items in speeches)
+                {
+                    ListCommands.Items.Add(items);
+                }
+            }
+            if (speech == "hide speeches" || speech == "hide it")
+            {
+                ListCommands.Visible = false;
+                string voice = "Speeches has been hidden.";
+                AssistantSpeechBubble(voice, true);
+            }
+        }
+
+        private void UserSpeechBubble(string words)
+        {
+            Label Bubble = new Label();
+            Bubble.Text = words;
+
+            Bubble.Margin = new Padding(13, 13, 150, 13);
+            Bubble.Padding = new Padding(8, 15, 8, 15);
+            Bubble.Anchor = AnchorStyles.Left;
+            Bubble.BackColor = Color.DodgerBlue;
+            Bubble.Size = new Size(135, 33);
+            Bubble.ForeColor = SystemColors.Control;
+            Bubble.AutoSize = true;
+            Bubble.Font = new Font("Elephant", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+
+            flowLayoutPanel1.Controls.Add(Bubble);
+            Console.WriteLine("User said: " + words);
+        }
+        private void AssistantSpeechBubble(string words, bool show)
+        {
+            Label Bubble = new Label();
+            Bubble.Text = words;
+
+            Bubble.Margin = new Padding(150, 13, 13, 13);
+            Bubble.Padding = new Padding(8, 15, 8, 15);
+            Bubble.Anchor = AnchorStyles.Right;
+            Bubble.BackColor = Color.SteelBlue;
+            Bubble.Size = new Size(135, 33);
+            Bubble.ForeColor = SystemColors.Control;
+            Bubble.AutoSize = true;
+            Bubble.Font = new Font("Elephant", 12F, FontStyle.Regular, GraphicsUnit.Point, ((byte)(0)));
+
+            flowLayoutPanel1.Controls.Add(Bubble);
+            VoiceAssistant.SpeakAsync(words);
+            Console.WriteLine("Voice Assistant said: " + words);
+        }
+
+        private void _recognizer_SpeechRecognized(object sender, SpeechDetectedEventArgs e)
+        {
+            RecTimeOut = 0;
+        }
+        private void startListening_SpeechRecognized(object sender, SpeechRecognizedEventArgs e)
+        {
+            string speech = e.Result.Text;
+            if (speech == "start")
+            {
+                startListening.RecognizeAsyncCancel();
+                VoiceAssistant.SpeakAsync("Greetings, I am back. What can I do for you?");
+                _recognizer.RecognizeAsync(RecognizeMode.Multiple);
+            }
+        }
+
+        private void SpeakTimer_Tick(object sender, EventArgs e)
+        {
+            if (RecTimeOut == 10)
+            {
+                _recognizer.RecognizeAsyncCancel();
+            }
+            else if (RecTimeOut > 10) 
+            {
+                SpeakTimer.Stop();
+                // restart the timer after hearing another speech
+                startListening.RecognizeAsync(RecognizeMode.Multiple);
+                RecTimeOut = 0;
+            }
+        }
+        private void _recognizer_AudioLevelUpdated(object sender, AudioLevelUpdatedEventArgs e)
+        {
+            SpeakBar.Value = e.AudioLevel;
+        }
+
+    }
+}
